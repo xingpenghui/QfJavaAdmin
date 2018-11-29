76e281f501332d12a54618e4c8dcbc0f9f0ea277
邢朋辉 <xingfei_work@163.com>
2018-11-29 18:32:35 +0800

    实现千锋郑州Java线上资源站后台管理系统

---
+++ src/main/java/com/zzjava/service/UserService.java
@@ -0,0 +1,15 @@
+package com.zzjava.service;
+
+import java.util.List;
+
+
+
+public interface UserService {
+
+//	//登录
+//	public User queryByNo(String no, String password);
+//	//修改密码
+//	public boolean updatePassword(User user);
+//	//新增
+//	public boolean insert(User user);
+}
