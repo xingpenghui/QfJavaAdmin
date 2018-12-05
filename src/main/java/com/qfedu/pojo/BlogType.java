997637c984e69f1054a5abda5531f4d7f78864bc
renr <renr_1981@aliyun.com>
2018-12-05 21:51:14 +0800

    新增

---
+++ src/main/java/com/qfedu/pojo/BlogType.java
@@ -0,0 +1,23 @@
+package com.qfedu.pojo;
+
+public class BlogType {
+
+    private Integer tid;
+    private String tname;
+
+    public Integer getTid() {
+        return tid;
+    }
+
+    public void setTid(Integer tid) {
+        this.tid = tid;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+}
