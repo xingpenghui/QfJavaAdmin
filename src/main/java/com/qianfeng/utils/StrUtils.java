82df4e9205d2722335059151129af53e7588ed67
renr <renr_1981@aliyun.com>
2018-12-09 17:55:55 +0800

    通过正则获取图片数据

---
+++ src/main/java/com/qianfeng/utils/StrUtils.java
@@ -0,0 +1,14 @@
+package com.qianfeng.utils;
+
+import java.util.List;
+
+public class StrUtils {
+    public static String listToString(List list, char separator){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<list.size(); i++){
+            sb.append(list.get(i)).append(separator);
+        }
+        return sb.toString().substring(0,sb.toString().length()-1);
+    }
+
+}
