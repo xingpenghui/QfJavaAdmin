386963fe72446cdb72b42c60fc5e734a5c7e146f
renr <renr_1981@aliyun.com>
2018-12-12 20:55:02 +0800

    生成html文件名

--- src/main/java/com/qianfeng/utils/StrUtils.java
+++ src/main/java/com/qianfeng/utils/StrUtils.java
@@ -1,6 +1,8 @@
 package com.qianfeng.utils;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 public class StrUtils {
     public static String listToString(List list, char separator){
@@ -9,6 +11,15 @@ public class StrUtils {
             sb.append(list.get(i)).append(separator);
         }
         return sb.toString().substring(0,sb.toString().length()-1);
+    }
+
+    public static String htmlName(){
+        Date date = new Date();
+        Random random = new Random();
+        int i = random.nextInt(10000);
+        // 时间戳和一个5位的数字组成，不够补0
+        String fileName = String.valueOf(date.getTime()) + String.format("%05d", i) + ".html";
+        return fileName;
     }
 
 }
